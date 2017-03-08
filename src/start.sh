2a2c6bc09b1d2a29ce713c779730260f4c96904c
Vanya Kiskin <snarovivan@gmail.com>
2017-03-08 23:47:12 +0300

    Исправлен скрипт запуска API

--- src/start.sh
+++ src/start.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-if [ ! -f api.pid ]; then
-    sh poll-sheduler.sh 5 `pwd` &>/dev/null & echo $! > api.pid || echo "Ошибка при запуске"
-else
-    echo "API уже запущен"
+API_DIR="/var/webapi/eripapi"
+PPP_NAME="erip_vpn"
+PPP_GATEWAY="10.54.4.1"
+PPP_REMOTE_HOST="10.54.10.2"
+
+if [[ $EUID -ne 0 ]]; then
+    echo "Только root может запускать API" >&2
+    exit 1
 fi
 
+if [ -f "$API_DIR/api.pid" ] && ps -p `cat api.pid` &>/dev/null; then
+    echo "API уже запущен"
+else
+    pppd call $PPP_NAME
+    
+    sudo -u eripapi sh poll-sheduler.sh 5 `pwd`&>/dev/null & echo $! > "$API_DIR/api.pid" || echo "Ошибка при запуске"
+fi
