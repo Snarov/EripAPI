a3d837b562cc5aca62ec555befc4c20241e301fa
Vanya Kiskin <snarovivan@gmail.com>
2017-04-12 16:05:57 +0300

    Добавлен автоподъем упавшего соединения с ЕРИП

--- src/poll-sheduler.sh
+++ src/poll-sheduler.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+PPP_NAME="erip_vpn"
+
 while :
 do
+    #проверям, отвалился ли коннект с ЕРИП и поднимаем, если нужно
+    if [ ! -d /sys/class/net/ppp1 ] ; then
+	pppd call $PPP_NAME
+    fi;
+    
     php $2/poll.php
     sleep $1;
 done
